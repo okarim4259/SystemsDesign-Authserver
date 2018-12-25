74125e5ac84c4b446e5d3cd5940ce61f551e43eb
okarim4259 <okarim4259@gmail.com>
2018-12-25 11:43:54 -0500

    added dotenv removed values

--- src/config/constants/constants.config.ts
+++ src/config/constants/constants.config.ts
@@ -1,13 +1,3 @@
-// export const KEYS = {
-//   ISSUER: "okarimdev",
-//   JWT_SECRET: "developmentJwtSecret",
-//   JWT_EXP: 360000,
-//   GOOGLE_PROJ_ID: "budgetplanner-214602",
-//   GOOGLE_API_KEY: "AIzaSyAv9WELjIqtsH8FgcG2egs4RdVQ4P2JRPY",
-//   GOOGLE_OAUTH2_CLIENT_ID:
-//     "287841994105-vr5v909v8217b02fl4pkbp4o0ab9h02r.apps.googleusercontent.com",
-//   GOOGLE_OAUTH2_CLIENT_SECRET: "mhPlFoLZdBsfTFVkl9RJ64dW"
-// };
 export const KEYS = {
   ISSUER: process.env.ISSUER,
   JWT_SECRET: process.env.JWT_SECRET,
